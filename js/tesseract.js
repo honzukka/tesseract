fa9e70572da8e134675ee086d989044cb1d6161e
honzukka <randomnonplayercharacter@gmail.com>
2020-06-07 20:32:47 +0300

    added edges

--- js/tesseract.js
+++ js/tesseract.js
@@ -8,6 +8,15 @@ class Tesseract {
 
         this.colors = [];
         this.vertices.forEach(v => this.colors.push(color));
+
+        this.edges = [];
+        for (let i = 0; i < this.vertices.length; i++) {
+            for (let j = i + 1; j < this.vertices.length; j++) {
+                if (countDifferentCoords(this.vertices[i], this.vertices[j]) === 1) {
+                    this.edges.push( [i, j] );
+                }
+            }
+        }
     }
 
     colorCube(i=0, color=[0, 1, 0]) {
@@ -22,13 +31,17 @@ class Tesseract {
     }
 }
 
-function arrayContains(array, val) {
-    for (arrayVal of array) {
-        if (arrayVal.toString() === val.toString()) {
-            return true;
-        }
+function countDifferentCoords(vertex1, vertex2) {
+    if (vertex1.length !== vertex2.length)
+        return 0;
+    
+    counter = 0;
+    for (let i = 0; i < vertex1.length; i++) {
+        if (vertex1[i] !== vertex2[i])
+            counter++;
     }
-    return false;
+
+    return counter;
 }
 
 let tesseract = new Tesseract();
@@ -52,36 +65,78 @@ window.addEventListener('resize', function() {
 
 // ---------------------------------------------------
 
-let geometry = new THREE.SphereBufferGeometry(0.05);
+// vertex geometry
+let vertexGeometry = new THREE.SphereBufferGeometry(0.05);
 let vertexMeshes = [];
 for (let i = 0; i < tesseract.vertices.length; i++) {
     let material = new THREE.MeshBasicMaterial( { color: tesseract.colors[i] } );
-    let mesh = new THREE.Mesh(geometry, material);
+    let mesh = new THREE.Mesh(vertexGeometry, material);
     vertexMeshes.push(mesh);
     scene.add(mesh);
 }
 
-let t = 0;
+// edge geometry
+let edgeLines = [];
+for (let i = 0; i < tesseract.edges.length; i++) {
+    let edge = tesseract.edges[i];
+    let material = new THREE.LineBasicMaterial( { color: tesseract.colors[edge[0]] } );
+    let points = [
+        new THREE.Vector3().fromArray(tesseract.vertices[edge[0]]),
+        new THREE.Vector3().fromArray(tesseract.vertices[edge[1]])
+    ];
+    let edgeGeometry = new THREE.BufferGeometry().setFromPoints( points );
+    let line = new THREE.Line( edgeGeometry, material );
+    edgeLines.push(line);
+    scene.add(line);
+}
+
+let t = 0.01;
 
 let render = function () {
     requestAnimationFrame( render );
 
-    for (let i = 0; i < tesseract.vertices.length; i++) {
+    for (let i = 0; i < vertexMeshes.length; i++) {
+        // vertex positions
         let v = tesseract.vertices[i];
         let vx = Math.cos(t) * v[0] - Math.sin(t) * v[3];
         let vw = Math.sin(t) * v[0] + Math.cos(t) * v[3];
-        let vy = Math.cos(t + Math.PI / 2) * v[1] - Math.sin(t - Math.PI / 2) * v[2];
-        let vz = Math.sin(t + Math.PI / 2) * v[1] + Math.cos(t - Math.PI / 2) * v[2];
+        let vy = Math.cos(t) * v[1] - Math.sin(t) * v[2];
+        let vz = Math.sin(t) * v[1] + Math.cos(t) * v[2];
         let stereography = 1 / (2 - vw);
         vertexMeshes[i].position.set(stereography * vx, stereography * vy, stereography * vz);
         //vertexMeshes[i].position.set(vx, vy, vz);
 
+        // update tesseract vertices
+        tesseract.vertices[i][0] = vx;
+        tesseract.vertices[i][1] = vy;
+        tesseract.vertices[i][2] = vz;
+        tesseract.vertices[i][3] = vw;
+
+        // color
         vertexMeshes[i].material.color.fromArray(tesseract.colors[i]);
     }
 
+    for (let i = 0; i < edgeLines.length; i++) {
+        let edge = tesseract.edges[i];
+        let vertex1 = tesseract.vertices[edge[0]];
+        let vertex2 = tesseract.vertices[edge[1]];
+        let positions = edgeLines[i].geometry.attributes.position.array;
+        let stereography1 = 1 / (2 - vertex1[3]);
+        let stereography2 = 1 / (2 - vertex2[3]);
+        positions[0] = vertex1[0] * stereography1;
+        positions[1] = vertex1[1] * stereography1;
+        positions[2] = vertex1[2] * stereography1;
+        positions[3] = vertex2[0] * stereography2;
+        positions[4] = vertex2[1] * stereography2;
+        positions[5] = vertex2[2] * stereography2;
+
+        edgeLines[i].geometry.attributes.position.needsUpdate = true;
+        
+        // TODO: colored cube should be green but it's red now
+        edgeLines[i].material.color.fromArray(tesseract.colors[edge[0]]);
+    }
+
     renderer.render(scene, camera);
-    t += 0.01;
 }
 
-//debugger;
 render();
